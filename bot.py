f85aeff9df2136e9de457b68761afb5fedaa4689
maxon390 <maxon390@gmail.com>
2026-03-02 13:20:16 +0200

    Add to try except bloke

--- bot.py
+++ bot.py
@@ -5,12 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()  # Тягне ваш ключ із .env
 
 # Ініціалізація клієнта з ключем із системи
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+try:
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# Приклад запиту
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{"role": "user", "content": "Привіт, як справи?"}]
-)
+    # Приклад запиту
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": "Привіт, як справи?"}]
+    )
+except Exception:
+    print("помилка")
 
 print(response.choices[0].message.content)
+

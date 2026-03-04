65185d4d654c0b7306975e5cce3214b340ceb0bf
Максим <maxon390@gmail.com>
2026-03-04 20:51:45 +0200

    add file gpt py

---
+++ gpt.py
@@ -0,0 +1,15 @@
+import os
+from openai import OpenAI
+# Ініціалізація клієнта з ключем із системи
+try:
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    # Приклад запиту
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": "Привіт, як справи?"}]
+    )
+except Exception:
+    print("помилка")
+
+print(response.choices[0].message.content)

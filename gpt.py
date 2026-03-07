232e390cdff159f7cd5f02d199e7b5602e50c4d7
Максим <maxon390@gmail.com>
2026-03-07 21:20:00 +0200

    add method clear message list

--- gpt.py
+++ gpt.py
@@ -37,3 +37,6 @@ class ChatGptService:
         self.message_list.append({"role": "system", "content": prompt_text})
         self.message_list.append({"role": "user", "content": message_text})
         return await self.send_message_list()
+
+    async def clear(self):
+        self.message_list.clear()

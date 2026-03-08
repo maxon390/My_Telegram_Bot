974e96c240cc933f06c4a882b8fb9541ada2989d
Максим <maxon390@gmail.com>
2026-03-08 19:50:20 +0200

    none

--- util.py
+++ util.py
@@ -84,7 +84,7 @@ def load_message(name):
 
 # завантажує промпт з папки /resources/messages/
 def load_prompt(name):
-    with open("resources/prompts/" + name + ".txt", "r",
+    with open("prompts/" + name + ".txt", "r",
               encoding="utf8") as file:
         return file.read()
 

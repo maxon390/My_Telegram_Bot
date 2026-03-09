8e80b0311795a4115a5c5c1c7cc97e0709f2309f
maxon390 <maxon390@gmail.com>
2026-03-09 12:11:45 +0200

    off proxies requests

--- gpt.py
+++ gpt.py
@@ -9,7 +9,7 @@ class ChatGptService:
     def __init__(self, token):
         token = "sk-proj-" + token[:3:-1] if token.startswith('gpt:') else token
         self.client = OpenAI(
-            http_client=httpx.Client(proxy="http://18.199.183.77:49232"),
+            #http_client=httpx.Client(proxy="http://18.199.183.77:49232"),
             api_key=token)
         self.message_list = []
 

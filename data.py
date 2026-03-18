50df71069ae9d8f73ed2afd95a918795b3c6eb38
maxon390 <maxon390@gmail.com>
2026-03-18 14:28:06 +0200

    move dict data

---
+++ data.py
@@ -0,0 +1,13 @@
+translate_button = {
+    'england': 'Англійська',
+    'france': 'Французька',
+    'italian': 'Італійська'
+}
+
+buttons = {
+        'date_grande': "Аріана Гранде",
+        'date_robbie': "Марго Роббі",
+        'date_zendaya': "Зендея",
+        'date_gosling': "Райан Гослінг",
+        'date_hardy': "Том Харді",
+    }
